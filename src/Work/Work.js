662535074f7712e6df17bd5dadd3b5ca0d0fe4d6
Rupali shinde <you@example.com>
2025-06-14 09:41:19 +0530

    Add files

--- src/Work/Work.js
+++ src/Work/Work.js
@@ -15,44 +15,33 @@ function Work() {
                                 <h3>Website <span>Portfolio</span></h3>
                                 {/* <p>Lorem ipsum dolor sit amet consectetur.</p> */}
                                 <div className="btn">
-                                    <button > <a style={{ color: "white" }} href="https://rupalivshindeportfolio.netlify.app/" target='_blank'>Preview</a></button>
-                                    <button>Github</button>
+                                    <button > <a  href="https://rupalivshindeportfolio.netlify.app/" target='_blank'>Preview</a></button>
+                                    <button> <a  href="https://github.com/Rupalivshinde/Port" target='_blank'>Github</a></button>
                                 </div>
                             </div>
 
                         </div>
 
-                        <div className="card-container">
-                            <div className="card">
-                                <img src="https://cdn.pixabay.com/photo/2023/08/23/09/39/sahara-8208094_640.jpg" alt="" />
-                                <h3>Website <span>Pixabay</span></h3>
-                                {/* <p>Lorem ipsum dolor sit amet consectetur.</p> */}
-                                <div className="btn">
-                                    <button > <a style={{ color: "white" }} href="https://66c4ad8d01beac00a088eb55--guileless-raindrop-7e4dbe.netlify.app/" target='_blank'>Preview</a></button>
-                                    <button>Github</button>
-                                </div>
-                            </div>
-
-                        </div>
+                       
                         <div className="card-container">
                             <div className="card">
                                 <img src="https://cdn.pixabay.com/photo/2023/07/27/07/42/cherry-cake-8152717_640.jpg" alt="" />
-                                <h3>Website <span>Restaurant </span></h3>
+                                <h3>Website <span>SEMRestaurant </span></h3>
                                 <div className="btn">
-                                    <button>Preview</button>
-                                    <button>Github</button>
+                                    <button > <a href='https://sem-restaurant.netlify.app/'>Preview</a>  </button>
+                                    <button> <a href='https://github.com/Rupalivshinde/Restaurant'>Github</a></button>
                                 </div>
                             </div>
 
                         </div>
                         <div className="card-container">
                             <div className="card">
-                                <img src="https://cdn.pixabay.com/photo/2020/09/15/20/35/hand-5574694_640.jpg" alt="" />
-                                <h3>Website <span>Wedding planner</span></h3>
+                                <img src="https://fitclub-1my.pages.dev/static/media/image3.69484cb0ee7687a8c28b.png" alt="" />
+                                <h3>Website <span>PrimeFit Club</span></h3>
                                 {/* <p>Lorem ipsum dolor sit amet consectetur.</p> */}
                                 <div className="btn">
-                                    <button>Preview</button>
-                                    <button>Github</button>
+                                    <button> <a href='https://rupalivshinde.github.io/gym/'>Preview</a></button>
+                                    <button> <a href='https://github.com/Rupalivshinde/gym'>Github</a></button>
                                 </div>
                             </div>
 
@@ -60,23 +49,34 @@ function Work() {
 
                         <div className="card-container">
                             <div className="card">
-                                <img src="https://cdn.pixabay.com/photo/2020/08/31/00/33/guitar-5531027_640.jpg" alt="" />
-                                <h3>Website <span>Song  Editor</span></h3>
+                                <img src="https://images.prismic.io/truekind/Zuq-1LVsGrYSvhqc_s764qjse3.jpg?auto=format,compress&w=640&fm=webp&q=95" alt="" />
+                                <h3>Website <span>Truekind</span></h3>
                                 {/* <p>Lorem ipsum dolor sit amet consectetur.</p> */}
                                 <div className="btn">
-                                    <button>Preview</button>
-                                    <button>Github</button>
+                                    <button> <a href='https://truekind-skincareweb.netlify.app/'>Preview</a></button>
+                                    <button><a href='https://github.com/Rupalivshinde/Truekind'>Github</a></button>
                                 </div>
                             </div>
 
                         </div>
                         <div className="card-container">
                             <div className="card">
-                                <img src="https://cdn.pixabay.com/photo/2016/03/30/05/41/music-1290087_640.jpg" alt="" />
-                                <h3>Website <span>Music player</span></h3>
+                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcHOkRCZ_JhvWPgW6KZWvOwJ1AdeAgQ0R-zw&s" alt="" />
+                                <h3>Website <span>Netflix-clone</span></h3>
                                 <div className="btn">
-                                    <button>Preview</button>
-                                    <button>Github</button>
+                                    <button> <a href='https://funny-souffle-c9edd1.netlify.app/'>Preview</a></button>
+                                    <button> <a href='https://github.com/Rupalivshinde/netflix-clone'>Github</a></button>
+                                </div>
+                            </div>
+
+                        </div>
+                         <div className="card-container">
+                            <div className="card">
+                                <img src="https://media.istockphoto.com/id/1007768414/photo/blue-sky-with-bright-sun-and-clouds.jpg?s=612x612&w=0&k=20&c=MGd2-v42lNF7Ie6TtsYoKnohdCfOPFSPQt5XOz4uOy4=" alt="" />
+                                <h3>Website <span>Weather</span></h3>
+                                <div className="btn">
+                                    <button> <a href='https://rupalivshinde.github.io/Weather/'>Preview</a></button>
+                                    <button> <a href='https://github.com/Rupalivshinde/Weather'>Github</a></button>
                                 </div>
                             </div>
 
